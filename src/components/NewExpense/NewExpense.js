e3440e1bcc5c66e5415e5f6101f0f044e2a90c0f
sameersheikh2 <sameersheikh819@gmail.com>
2022-06-23 17:23:20 +0530

    added filter by years and dynamic showing new expense list

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,18 +2,18 @@ import React from 'react'
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 
-const NewExpense = () => {
-    const SaveExpenseDataHandler = (enteredExpenseData) =>{
+const NewExpense = (props) => {
+    const ExpenseDataHandler = (enteredExpenseData) =>{
        const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
-        console.log(expenseData)
+        props.onAddExpense(expenseData)
     };
 
   return (
     <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler}/>
+        <ExpenseForm onSaveExpenseData={ExpenseDataHandler}/> 
     </div>
   )
 }
